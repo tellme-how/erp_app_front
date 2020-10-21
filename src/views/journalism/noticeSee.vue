14e6754078bf341edf947541fab198d1ce8b62c7
孟鹏飞 <437994940@qq.com>
2020-10-21 13:11:14 +0800

    table显示表格

--- src/views/journalism/noticeSee.vue
+++ src/views/journalism/noticeSee.vue
@@ -14,10 +14,7 @@
 			</van-row>
 		</van-cell-group>
 		<van-cell-group>
-			<!-- <quill-editor style="width: 100%;height:400px" @contentData="editorChange($event)" disabled v-model="context.fcontent" ref="myQuillEditor" :options="editorOption" ></quill-editor> -->
-			<editor :value="context.fcontent" @contentData="editorChange($event)" style="height: 400px"></editor>
-			<!-- <editor :value="formdata.fcontent" @contentData="editorChange($event)" style="height: 400px"></editor> -->
-			<!--<div style="width: 100%;" v-html="context.fcontent"></div>-->
+			<editor v-if="showTest" :value="context.fcontent" @contentData="editorChange($event)"></editor>
 		</van-cell-group>
 		<van-cell-group v-for="(item,index) in fileList" :key="index">
 			<van-row style="height: auto;line-height: auto;text-align:center;padding: 10px;" @click="aaa(item)">
@@ -28,13 +25,9 @@
 </template>
 
 <script>
-	import { quillEditor } from 'vue-quill-editor'; //调用编辑器
-	import Quill from 'quill'
-	import 'quill/dist/quill.snow.css'
 	import editor from './test'
 	export default {
 		components: {
-			quillEditor,
 			editor
 		},
 		data() {
@@ -42,31 +35,7 @@
 				context: {},
 				editorOption: this.$GLOBAL.editorOptionNull,
 				fileList: [],
-				// ------------文本编辑器---BEGIN-------------------------------------------------------------------------
-            // fcontent: '',
-            editorOption: {
-                placeholder: '请输入内容',
-                modules: {
-                    toolbar: [
-                        ['bold', 'italic', 'underline', 'strike'], //加粗，斜体，下划线，删除线
-                        ['blockquote', 'code-block'], //引用，代码块
-                        [{ header: 1 }, { header: 2 }], // 标题，键值对的形式；1、2表示字体大小
-                        [{ list: 'ordered' }, { list: 'bullet' }], //列表
-                        [{ script: 'sub' }, { script: 'super' }], // 上下标
-                        [{ indent: '-1' }, { indent: '+1' }], // 缩进
-                        [{ direction: 'rtl' }], // 文本方向
-                        [{ size: ['small', false, 'large', 'huge'] }], // 字体大小
-                        [{ header: [1, 2, 3, 4, 5, 6, false] }], //几级标题
-                        [{ color: [] }, { background: [] }], // 字体颜色，字体背景颜色
-                        [{ font: [] }], //字体
-                        [{ align: [] }], //对齐方式
-                        ['clean'], //清除字体样式
-                        ['image'], //上传图片、上传视频
-                    ]
-                }
-            },
-            atctiveName: 'first',
-            // ------------文本编辑器 ---END----------------------------------------------------------------------------
+				showTest: false,
 			};
 		},
 		created() {
@@ -84,6 +53,7 @@
 			}).then(data => {
 				this.context = data.data.data
 				this.$store.commit("titleShow", this.context.fname)
+				this.showTest = true
 			})
 			let creator = localStorage.getItem('ms_userId');
 			if(creator) {
@@ -102,27 +72,26 @@
 		methods: {
 			aaa(row) {
 				this.$api.myDesk.getHtmlPreviewAttachmentById({
-					id:row.id
+					id: row.id
 				}).then(data => {
 					this.$router.push({
-						name : "preview",
-						params:{
-							context : data.data.data,
-							titleShow : row.fileName
+						name: "preview",
+						params: {
+							context: data.data.data,
+							titleShow: row.fileName
 						}
 					})
 				})
 			},
 			//编辑器的页面显示效果
 			editorChange(aaa) {
-				debugger
 				this.$nextTick(() => {
-				if("<p>undefined</p>" == aaa){
-					this.context.fcontent = '';
-				} else {
-					this.context.fcontent = aaa;
-				}
-				this.$set(this.context, "fcontent", this.context.fcontent);
+					if("<p>undefined</p>" == aaa) {
+						this.context.fcontent = '';
+					} else {
+						this.context.fcontent = aaa;
+					}
+					this.$set(this.context, "fcontent", this.context.fcontent);
 				});
 
 			},
--- src/views/journalism/test.vue
+++ src/views/journalism/test.vue
@@ -52,69 +52,14 @@
 					modules: {
 						toolbar: {
 							container: [
-								['bold', 'italic', 'underline', 'strike'],
+								['bold'], //加粗
 								[{
-									header: 1
-								}, {
-									header: 2
-								}],
-								[{
-									list: 'ordered'
-								}, {
-									list: 'bullet'
-								}],
-								[{
-									indent: '-1'
-								}, {
-									indent: '+1'
-								}],
-								[{
-									color: []
-								}, {
-									background: []
-								}],
-								[{
-									font: []
+									'color': []
 								}],
 								[{
 									align: []
-								}],
-								['clean'],
-								['image'],
-								[{
-										table: 'TD'
-									},
-									{
-										'table-insert-row': 'TIR'
-									},
-									{
-										'table-insert-column': 'TIC'
-									},
-									{
-										'table-delete-row': 'TDR'
-									},
-									{
-										'table-delete-column': 'TDC'
-									}
-								]
+								}], //对齐方式
 							],
-							handlers: {
-								table: function(val) {
-									this.quill.getModule('table').insertTable(2, 3)
-								},
-								'table-insert-row': function() {
-									this.quill.getModule('table').insertRowBelow()
-								},
-								'table-insert-column': function() {
-									this.quill.getModule('table').insertColumnRight()
-								},
-								'table-delete-row': function() {
-									this.quill.getModule('table').deleteRow()
-								},
-								'table-delete-column': function() {
-									this.quill.getModule('table').deleteColumn()
-								}
-							}
 						},
 						table: true
 					},
@@ -147,28 +92,16 @@
 		mounted() {
 			const dom = this.$el.querySelector('.editor')
 			this.quill = new Quill(dom, this.options)
-			if(typeof(this.value) == "undefined"){
+			if(typeof(this.value) == "undefined") {
 				this.quill.root.innerHTML = ""
-			}else{
+			} else {
 				this.quill.root.innerHTML = this.value
 			}
 			this.quill.on('text-change', () => {
-				debugger
 				this.$emit('contentData', this.quill.root.innerHTML)
 			})
-			this.$el.querySelector(
-				'.ql-table-insert-row'
-			).innerHTML = `<svg t="1591862376726" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6306" width="18" height="200"><path d="M500.8 604.779L267.307 371.392l-45.227 45.27 278.741 278.613L779.307 416.66l-45.248-45.248z" p-id="6307"></path></svg>`
-			this.$el.querySelector(
-				'.ql-table-insert-column'
-			).innerHTML = `<svg t="1591862238963" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6509" width="18" height="200"><path d="M593.450667 512.128L360.064 278.613333l45.290667-45.226666 278.613333 278.762666L405.333333 790.613333l-45.226666-45.269333z" p-id="6510"></path></svg>`
-			this.$el.querySelector(
-				'.ql-table-delete-row'
-			).innerHTML = `<svg t="1591862253524" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6632" width="18" height="200"><path d="M500.8 461.909333L267.306667 695.296l-45.226667-45.269333 278.741333-278.613334L779.306667 650.026667l-45.248 45.226666z" p-id="6633"></path></svg>`
-			this.$el.querySelector(
-				'.ql-table-delete-column'
-			).innerHTML = `<svg t="1591862261059" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6755" width="18" height="200"><path d="M641.28 278.613333l-45.226667-45.226666-278.634666 278.762666 278.613333 278.485334 45.248-45.269334-233.365333-233.237333z" p-id="6756"></path></svg>`
 			this.addQuillTitle()
+			this.quill.enable(false)
 
 		},
 		activated() {
@@ -176,3 +109,9 @@
 		}
 	}
 </script>
+<style scoped="scoped">
+	/deep/.ql-toolbar {
+		display: none;
+	}
+	
+</style>
